8563667e5fe8a4bc8b37bea1a86dee2dbda2efe1
Yusuf Altunoymak <yusuf.altunoymak79@gmail.com>
2023-04-16 16:09:48 +0300

    return

--- app/src/main/java/com/example/splash/ilanDetayFragment.kt
+++ app/src/main/java/com/example/splash/ilanDetayFragment.kt
@@ -68,8 +68,6 @@ class ilanDetayFragment : Fragment() {
         Picasso.get().load(gelenphoto).into(binding.ilanDetayimage)
         println(gelenBaslik)
         println(gelenBaslik)
-        println("asdfsdf")
-
         // binding.ilanDetayimage.setImageURI()
 
 
