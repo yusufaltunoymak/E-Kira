71bf380ce7f164bf1e6ffd65624a4793967b5262
Eren Sakrak <eren.sakrak1453@gmail.com>
2023-04-22 14:11:59 +0300

    fork11

--- app/src/main/java/com/example/splash/ilanDetayFragment.kt
+++ app/src/main/java/com/example/splash/ilanDetayFragment.kt
@@ -80,7 +80,7 @@ class ilanDetayFragment : Fragment() {
             val action = ilanDetayFragmentDirections.ilantomain("sa")
             Navigation.findNavController(it).navigate(action)
         }
-    println("22222")
+    println("222222")
 
 
     }
