e4ee4051ee819fb09609a40b7f1a44ca7fb83200
Eren Sakrak <eren.sakrak1453@gmail.com>
2023-05-15 21:02:10 +0300

    profil ve bakiye

--- app/src/main/java/com/example/splash/BakiyeCekFragment.kt
+++ app/src/main/java/com/example/splash/BakiyeCekFragment.kt
@@ -11,11 +11,7 @@ import android.view.ViewGroup
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [BlankFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 class BakiyeCekFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
